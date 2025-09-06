4145a0509bdb74e1cf1e44bf5e947c37d92e1b56
yuukikuno <kunoyuuki@proton.me>
2025-09-06 23:00:57 +0200

    Only launch zellij in ghostty

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,14 +1,24 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-    # fish
+    # fzf
+    #fzf --fish | source
+
+    # atuin
     atuin init fish | source
     bind up _atuin_bind_up
-
+    
+    if test "$TERM_PROGRAM" = "ghostty"
     # zellij
-    set ZELLIJ_AUTO_EXIT true
-    eval (zellij setup --generate-auto-start fish | string collect)
+        set ZELLIJ_AUTO_EXIT true
+        eval (zellij setup --generate-auto-start fish | string collect)
+    end
 
     # mise
     mise activate fish | source
+
+    # zoxide
+    zoxide init fish | source
 end
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
